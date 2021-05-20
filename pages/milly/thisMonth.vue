63bb04a3fcaa6281538bfd6677f55e1ba5a8a85b
milly98 <chlalsrud0923@gmail.com>
2021-05-20 18:15:24 +0900

    날짜 범위 설정 [Milly]

--- pages/milly/thisMonth.vue
+++ pages/milly/thisMonth.vue
@@ -1,27 +1,6 @@
 <template>
   <div class="container">
     <div class="title"> Milly's calendar</div>
-    <h2> {{ today.getMonth() + 1 }} 월 </h2>
-    <table>
-      <thead>
-      <tr>
-        <td>일</td>
-        <td>월</td>
-        <td>화</td>
-        <td>수</td>
-        <td>목</td>
-        <td>금</td>
-        <td>토</td>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="week in setCalendar">
-        <td v-for="(date,idx) in week">{{date.date}}</td>
-      </tr>
-      </tbody>
-    </table>
-
-
     <input v-model="year"/>
     <div class="month margin50">
       <div @click="month > 0 ? month-- : (month = 11, year--)"> < </div>
@@ -33,7 +12,7 @@
         <span class="day" v-for="day in day">{{ day }}</span>
       </div>
       <div class="week" v-for="week in setCalendar">
-        <span class="date" v-for="(date,idx) in week">{{date.date}}</span>
+        <span @click="selectedDate = date.date; dateRange()" class="date" v-for="(date,idx) in week">{{date.date}}</span>
       </div>
     </div>
   </div>
@@ -49,7 +28,9 @@ export default {
       today : new Date(),
       year : new Date().getFullYear(),
       month : new Date().getMonth(),
-      date : new Date().getDate()
+      selectedDate : new Date().getDate(),
+      startDay : null,
+      endDay : null,
     };
   },
   mounted() {
@@ -58,7 +39,7 @@ export default {
   computed:{
     setCalendar(){
       let calendarData = [];
-      const standardDate = new Date(this.year, this.month, this.date);
+      const standardDate = new Date(this.year, this.month, this.selectedDate);
       standardDate.setDate(1);
       const firstDayOfMonth = standardDate.getDay();
       standardDate.setMonth(+1);
@@ -81,9 +62,45 @@ export default {
         calendarData.push(dateDay);
         dateDay = [];
       }
+      // console.log(calendarData);
       return calendarData
-    }
+    },
+
   },
+  methods: {
+    // isHoliday(){
+    //   // 주말, 공휴일은 클래스를 추가해서 빨간색으로 날을 표시하도록 해야 함.
+    //   // 주말은 day === 0 || day === 6 일요일 || 토요일
+    //   // 공휴일은 연월일 을 'YYYYMMDD' 형식으로 저장 해놓기 => 그럼 calendar 에 'YYYYMMDD' 형식으로 저장을 해야 하는가 비교 할 때마다 만드는게 나은가? 무엇이 효율적...?
+    //
+    //   const holidayList = ['20210501', '20210502',
+    //     '20210508', '20210509',
+    //     '20210515', '20210516',
+    //     '20210522', '20210523',
+    //     '20210529', '20210530']
+    //   console.log(`${this.year}${this.month+1}${this.date}`)
+    //   if(holidayList.includes(`${this.year}${this.month+1}${this.date}`)) return true;
+    //   return false;
+    // },
+    dateRange(){
+      // 초반에 start, end 다 비어있음.
+      // 두개 다 비어있는 경우 ? start에 선택 된 날 넣음.
+      // start보다 선택된게 크면 ? start/end 에 넣음
+      // start보다 선택된게 작으면 ? start/end 에 넣음
+
+      if(this.startDay && Number(this.startDay) < Number(new Date(this.year, this.month, this.selectedDate)) && Number(this.startDay) === Number(this.endDay)){
+        console.log("endDay 가 바뀌어야 함")
+        this.endDay = new Date(this.year, this.month, this.selectedDate)
+      }else {
+        console.log("둘 다 바뀌어야 함")
+        this.startDay = new Date(this.year, this.month, this.selectedDate)
+        this.endDay = new Date(this.year, this.month, this.selectedDate)
+      }
+
+      console.log('startDay > ', this.startDay);
+      console.log('endDay > ', this.endDay);
+    }
+  }
 }
 </script>
 
