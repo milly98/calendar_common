4abd84214736f5ab60758d99dd39271138653a6b
milly98 <chlalsrud0923@gmail.com>
2021-05-24 11:30:20 +0900

    axios를 통한 공휴일 api 연동

--- pages/milly/index.vue
+++ pages/milly/index.vue
@@ -6,6 +6,7 @@
 
 <script>
 import Calendar from "../../components/Milly/Calendar";
+import axios from 'axios';
 
 export default {
   components: {Calendar},
@@ -14,6 +15,23 @@ export default {
     };
   },
   mounted() {
+    console.log('mounted')
+    this.getRestDate('2021','05');
+
+  },
+  methods:{
+    getRestDate(year, date){
+      // this.$axios.get(`/api/getRestDeInfo?serviceKey=b%2Bs0OmI7q0%2B5rtcOg9cPQ8NlFZ0eWbdVKEJdA1riLNYJ3M7y%2Brmyxe5jHfCWC8NR4CzemZvBZ%2B2EALje81R85A%3D%3D&solYear=${year}&solMonth=${date}`).then((resp) => {
+      //   console.log('resp >> ', resp);
+      // });
+
+      this.$apis.getRestDeInfo({
+        year: year,
+        date: date,
+      }).then((resp) => {
+        console.log('resp resp resp  >>  ', resp)
+      });
+    }
   },
 }
 </script>
